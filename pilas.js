60d5687a8708c9bfb2c389a0fa644c14cedd3f46
Ilse Centeno <Ilse Centeno>
2017-03-14 08:52:29 -0600

    Ejercicio1

---
+++ pilas.js
@@ -0,0 +1,27 @@
+var nombres = [];
+
+
+var nombre = new Stack();{
+    nombre.push("Isaac");
+    nombre.push("Juan");
+    nombre.push("Gisela");
+    nombre.push("Flor");
+    nombre.push("Gerardo");
+}
+
+var apellido = new Stack ();{
+    apellido.push("Juárez");
+    apellido.push("Méndez");
+    apellido.push("Reyes");
+    apellido.push("Guzmán");
+    apellido.push("Hernández");
+  
+  return apellido;
+};
+
+function nombreCompleto (nombre, apellido){
+     nombre.pop() + " " + apellido.pop();
+}
+return nombres; 
+
+
